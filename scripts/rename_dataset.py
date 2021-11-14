672568fc65c1ce2dcff5c12cb539c095509fb92c
Iker <igarciaf896@gmail.com>
2021-11-14 09:59:54 -0400

    Add tqdm

--- scripts/rename_dataset.py
+++ scripts/rename_dataset.py
@@ -1,11 +1,12 @@
 import os
 import glob
 import argparse
+from tqdm import tqdm
 
 
 def rename_dataset(dataset_dir: str):
     dataset_files = glob.glob(os.path.join(dataset_dir, "*.jpeg"))
-    for dataset_file in dataset_files:
+    for dataset_file in tqdm(dataset_files):
         metadata = os.path.basename(dataset_file)[:-5]
         imageno, key = metadata.split("_")
 
