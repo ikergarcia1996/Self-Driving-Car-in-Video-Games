3a8e7f18522451b0a40fe980626a22439a7f306d
ikergarciaf <igarciaf896@gmail.com>
2021-12-03 01:05:11 +0100

    Manually detach tensors in training return

--- model.py
+++ model.py
@@ -1154,7 +1154,7 @@ class Tedd1104ModelPL(pl.LightningModule):
             "Train/running_loss", self.running_loss / self.total_batches, sync_dist=True
         )
 
-        return {"preds": preds, "y": y, "loss": loss}
+        return {"preds": preds.detach(), "y": y, "loss": loss}
 
     def training_step_end(self, outputs):
         self.train_accuracy(outputs["preds"], outputs["y"])
@@ -1359,7 +1359,7 @@ class Tedd1104ModelPLForImageReordering(pl.LightningModule):
         self.log(
             "Train/running_loss", self.running_loss / self.total_batches, sync_dist=True
         )
-        return {"preds": torch.argmax(preds, dim=-1), "y": y, "loss": loss}
+        return {"preds": torch.argmax(preds.detach(), dim=-1), "y": y, "loss": loss}
 
     def training_step_end(self, outputs):
         self.train_accuracy(outputs["preds"], outputs["y"])
