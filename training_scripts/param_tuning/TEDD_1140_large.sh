8690140adeb46be5a625557bbf2c8402c342d4a6
ikergarciaf <igarciaf896@gmail.com>
2022-05-09 17:18:12 +0200

    Change hparams

--- training_scripts/param_tuning/TEDD_1140_large.sh
+++ training_scripts/param_tuning/TEDD_1140_large.sh
@@ -5,22 +5,22 @@ python3 train.py --train_new \
   --encoder_type transformer \
   --dataloader_num_workers 32 \
   --batch_size 8 \
-  --accumulation_steps 4 \
+  --accumulation_steps 1 \
   --max_epochs 20 \
   --cnn_model_name efficientnet_v2_l \
   --num_layers_encoder 6 \
-  --embedded_size 384 \
+  --embedded_size 512 \
   --learning_rate 3e-5 \
   --mask_prob 0.2 \
   --hide_map_prob 0.0 \
   --dropout_cnn_out 0.4 \
   --dropout_encoder 0.2 \
   --dropout_encoder_features 0.5 \
-  --label_smoothing 0.8 \
+  --label_smoothing 0.1 \
   --control_mode keyboard \
   --val_check_interval 0.25 \
   --precision "16"  \
-  --devices 2 \
+  --devices 4 \
   --strategy "ddp_find_unused_parameters_false"
 
 
@@ -31,7 +31,7 @@ python3 train.py --train_new \
   --encoder_type transformer \
   --dataloader_num_workers 32 \
   --batch_size 8 \
-  --accumulation_steps 4 \
+  --accumulation_steps 2 \
   --max_epochs 20 \
   --cnn_model_name efficientnet_v2_l \
   --num_layers_encoder 6 \
