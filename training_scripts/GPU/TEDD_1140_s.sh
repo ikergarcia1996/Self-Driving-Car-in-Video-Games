be5848d9933e92d2c1a87721357ec7bf566930f9
Iker <igarciaf896@gmail.com>
2022-06-03 14:20:21 +0200

    GPU final training scripts

--- training_scripts/GPU/TEDD_1140_s.sh
+++ training_scripts/GPU/TEDD_1140_s.sh
@@ -1,40 +1,46 @@
 #!/bin/bash
-#SBATCH --job-name=base
+#SBATCH --job-name=TEDD_S
 #SBATCH --cpus-per-task=40
-#SBATCH --gres=gpu:4
-#SBATCH --mem=300G
-#SBATCH --output=base.out
-#SBATCH --error=base.err
+#SBATCH --gres=gpu:1
+#SBATCH --mem=200G
+#SBATCH --output=TEDD_S.out
+#SBATCH --error=TEDD_S.err
 
 source /ikerlariak/igarcia945/envs/pytorch-tximista/bin/activate
 
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
 export LANGUAGE=en_US.UTF-8
+export OMP_NUM_THREADS=16
 
 cd ../../
 
 python3 train.py --train_new \
   --train_dir ../gtaai_datasets/train \
   --val_dir  ../gtaai_datasets/dev \
-  --output_dir models/tedd_1104_base \
+  --output_dir models/tedd_1104_S \
   --encoder_type transformer \
-  --dataloader_num_workers 64 \
-  --batch_size 8 \
-  --accumulation_steps 2 \
+  --dataloader_num_workers 32 \
+  --batch_size 32 \
+  --accumulation_steps 1 \
   --max_epochs 20 \
-  --cnn_model_name efficientnet_v2_m \
-  --num_layers_encoder 4 \
-  --embedded_size 512 \
-  --learning_rate 5e-5 \
+  --cnn_model_name efficientnet_v2_s \
+  --num_layers_encoder 2 \
+  --embedded_size 384 \
+  --learning_rate 1e-5 \
+  --optimizer_name adafactor \
+  --scheduler_name cosine \
+  --warmup_factor 0.05 \
   --mask_prob 0.2 \
+  --hide_map_prob 0.0 \
   --dropout_cnn_out 0.3 \
   --dropout_encoder 0.1 \
   --dropout_encoder_features 0.3 \
+  --label_smoothing 0.1 \
   --control_mode keyboard \
   --val_check_interval 0.25 \
-  --precision "16" \
-  --devices 4 \
+  --precision "16"  \
+  --devices 1 \
   --strategy "ddp_find_unused_parameters_false"
 
 
