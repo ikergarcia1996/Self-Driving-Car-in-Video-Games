8edf6e93ca2aba5ad8c2c508bccb23522548eb75
ikergarciaf <igarciaf896@gmail.com>
2023-11-05 11:20:28 +0100

    Improve configs

---
+++ training_scripts/pretrain/TEDD1104-xl.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --job-name=pretrain-xl
+#SBATCH --cpus-per-task=128
+#SBATCH --gres=gpu:4
+#SBATCH --mem=128G
+#SBATCH --output=.slurm/pretrain-xl.out.txt
+#SBATCH --error=.slurm/pretrain-xl.err.txt
+
+source /ikerlariak/igarcia945/envs/pytorch2/bin/activate
+
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LANGUAGE=en_US.UTF-8
+export TOKENIZERS_PARALLELISM=true
+export TRANSFORMERS_NO_ADVISORY_WARNINGS=true
+export WANDB_PROJECT=TEDD1104
+export OMP_NUM_THREADS=16
+
+CONFIGS_FOLDER="configs/pretrain"
+
+torchrun --standalone --master_port 37223 --nproc_per_node=4 train_TEDD1104.py ${CONFIGS_FOLDER}/TEDD1104-xl.yaml
