51f4ae5dd9dded3abd3bd75e4c2e548b42c6e372
ikergarciaf <igarciaf896@gmail.com>
2023-11-05 22:04:55 +0100

    pretrain+video-classification

---
+++ training_scripts/pretrain+video-classification/TEDD1104-base.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --job-name=pretrain+video-classification-base
+#SBATCH --cpus-per-task=128
+#SBATCH --gres=gpu:4
+#SBATCH --mem=128G
+#SBATCH --output=.slurm/pretrain+video-classification-base.out.txt
+#SBATCH --error=.slurm/pretrain+video-classification-base.err.txt
+
+source /ikerlariak/igarcia945/envs/pytorch2/bin/activate
+
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LANGUAGE=en_US.UTF-8
+export TOKENIZERS_PARALLELISM=true
+export TRANSFORMERS_NO_ADVISORY_WARNINGS=true
+export WANDB_PROJECT=TEDD1104
+export OMP_NUM_THREADS=16
+
+CONFIGS_FOLDER="configs/pretrain+video-classification"
+
+torchrun --standalone --master_port 37223 --nproc_per_node=4 train_TEDD1104.py ${CONFIGS_FOLDER}/TEDD1104-base.yaml
